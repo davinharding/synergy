3f3555f7f5d71d2063195ffa25eaa970604986e5
Davin Harding <davinlharding@gmail.com>
2018-10-09 19:16:02 -0400

    rebundle

--- app/javascript/packs/TestPack.js
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import TestComponent from '../bundles/TestComponent/components/TestComponent';
-
-// This is how react_on_rails can see the TestComponent in the browser.
-ReactOnRails.register({
-  TestComponent,
-});
