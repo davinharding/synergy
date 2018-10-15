c516f0c66cc8899cf22f651c7740383587bd62e2
Davin Harding <davinlharding@gmail.com>
2018-10-15 16:20:57 -0400

    Added psuedo auto refresh functionality

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,12 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @notifications = Notificaton.where(recipient: current_user).unread
+    @notifications = Notification.where(recipient: current_user.profile).unread.count
+    @profiles = current_user.profile&.notifications&.unread&.map(&:actor)&.uniq || []
+    respond_to do |format|
+      format.json do 
+        render json: { notifications: @notifications, profiles: @profiles }
+      end
+    end
   end
 end
