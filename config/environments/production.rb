d502207029713da522a7ee9729b7fedddcd067f7
Davin Harding <davinlharding@gmail.com>
2018-10-16 20:49:18 -0400

    fixed production configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,22 +96,17 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    s3_host_name: "synergy.dokku-hosted.thruhere.net",
     s3_credentials: {
-      bucket: ENV['S3_BUCKET_NAME'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      s3_region: ENV['AWS_REGION'],
+      bucket: ENV["S3_BUCKET_NAME"],
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+    },
+    s3_region: ENV["AWS_REGION"],
+    s3_protocol: :https,
+    ssl_verify_peer: false,
+    s3_options: {
+      endpoint: "https://synergy.dokku-hosted.thruhere.net/"
     }
-  }
-
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    }
-  }
 
 end
